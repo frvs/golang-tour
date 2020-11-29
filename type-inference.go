d620f749bef75e5cb4cf602a285b7a382884e730
Lucas Frois <lucas.frois@lambda3.com.br>
2020-11-29 05:09:20 -0300

    init

---
+++ type-inference.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 
+	x := 3.12
+	y := 3.12323251232
+	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("x is of type %T\n", x)
+	fmt.Printf("y is of type %T\n", y)
+}
